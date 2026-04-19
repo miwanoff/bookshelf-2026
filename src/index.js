81c2a9625008ad59448619faf72e857707296306
miwanoff <marina.iva.noffa@gmail.com>
2026-04-19 12:52:48 +0300

    Add App

--- src/index.js
+++ src/index.js
@@ -2,17 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import booksData from "./books.js";
 
-function Hello() {
+// function Hello() {
+//   return (
+//     <div>
+//       <h1 style={{ color: "red" }}>Hello, world!</h1>
+//       <h2>{booksData[0].name}</h2>
+//     </div>
+//   );
+// }
+
+const App = () => {
   return (
     <div>
-      <h1 style={{ color: "red" }}>Hello, world!</h1>
-      <h2>{booksData[0].name}</h2>
+      <h4>{booksData[0].name}</h4>
     </div>
   );
-}
+};
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <Hello />
+    <App />
 );
 
