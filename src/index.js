160dd0e2b21a52aeb423312c8799b1416f55fa21
miwanoff <marina.iva.noffa@gmail.com>
2026-04-19 12:50:26 +0300

    Add books.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import booksData from "./books.js";
 
 function Hello() {
   return (
     <div>
-      <h1>Hello, world!</h1>;
+      <h1 style={{ color: "red" }}>Hello, world!</h1>
+      <h2>{booksData[0].name}</h2>
     </div>
   );
 }
